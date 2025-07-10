ef062dad5083c4faf2acc51e7b28a3d81f3c38d6
anano303 <124179773+anano303@users.noreply.github.com>
2025-07-11 03:22:22 +0400

    Revert "Reapply "ვერცელ""
    This reverts commit b23a47d1729a68d8fb8e249d5ac51ce32999b17b.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,5 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -12,7 +7,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
-  ],
+    provideRouter(routes)
+  ]
 };
